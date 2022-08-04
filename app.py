34d56fc9418887f5cb69e3d764fe8e910cdda003
janazu23-meet <109899957+janazu23-meet@users.noreply.github.com>
2022-08-04 10:14:29 +0300

    completed i think

--- app.py
+++ app.py
@@ -18,11 +18,48 @@ auth = firebase.auth()
 db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = "jkbkigyiugyigiy"
 
-@app.route('/')
-def about():
-    return render_template('index.html')
 
+@app.route('/members_details')
+def members_details():
+	return render_template('speaker-details.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def signin():
+	error = ""
+	if request.method == 'POST':
+
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+			return redirect(url_for('index'))
+		except: 
+		
+			error = "Authentication failed"
+	return render_template("signin.html")
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		login_session['user'] = auth.create_user_with_email_and_password(email, password)
+		user = {"name": request.form['name'], "email": request.form["email"], "password": request.form["password"]}
+		db.child("users").child(login_session["user"]["localId"]).set(user)
+		try:
+			return redirect(url_for('index'))
+		except:
+			error = "Authentication failed"
+		return render_template("signup.html", error=error)
+	return render_template("signup.html")
+
+@app.route('/index')
+def index():
+	username = db.child("users").child(login_session['user']['localId']).get().val()['name']
+	return render_template('index.html', username=username)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
